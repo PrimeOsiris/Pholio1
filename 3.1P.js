8812db8ed3bc3fde939e36695e04612162d2df13
PrimeOsiris <109208283+PrimeOsiris@users.noreply.github.com>
2022-09-08 07:47:14 +1000

    Add files via upload

---
+++ 3.1P.js
@@ -0,0 +1,19 @@
+const greet = "Hello Viewer";
+console.log(greet);
+
+greet = text.toUpperCase();
+console.log(greet);
+
+greet = text.toLowerCase();
+console.log(greet);
+
+greet = text.replace(/Viewer/g/i,"Kings");
+console.log(greet);
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+greet = text.length;
+console.log(greet);
+
+let str = "Apple, Banana, Kiwi";
+greet = str.slice(7,13); 
+console.log(greet);
---
+++ 3.2P.js
@@ -0,0 +1,12 @@
+const welcome = "G'day Mate";
+console.log(welcome);
+console.log(welcome.charAt(0)); //G
+
+const numb = [1,2,3,4,5,6,7,8,9,0]; 
+const parts = ["G","'","d","a","y"," ","M","a","t","e"];
+
+console.log(numb.toString());
+console.log(parts.join(" "));
+console.log(numb.pop());
+console.log(parts.shift());
+console.log(parts.unshift("M"));
